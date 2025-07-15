38cdb7b7ceef60e4f6c0452e1c8a598bdf690a30
mishraji874 <developer.adityamsr@gmail.com>
2025-07-15 18:28:04 +0530

    part-c of part-1 completed

--- Part-1/part-c-component-state-and-event-handlers/src/App.jsx
+++ Part-1/part-c-component-state-and-event-handlers/src/App.jsx
@@ -1,35 +1,205 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Component helper function
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
       </p>
-    </>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
   )
 }
 
-export default App
+// Destructuring the props object
+const HelloDestruct1 = (props) => {
+
+  // const name = props.name
+  // const age = props.age
+
+  /// another way to destructure
+  const { name, age } = props
+
+
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const HelloDestruct2 = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App;
+
+// Page re-rendering example
+const AppReRender = (props) => {
+  const {counter} = props;
+  return (
+    <div>{counter}</div>
+  )
+}
+
+// Stateful component example
+import { useState } from 'react';
+
+const AppState = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
+
+  return (
+    <div>{counter}</div>
+  )
+}
+
+// Event handler example
+const AppEventHandler = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    console.log('Button clicked');
+  }
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>
+        plus
+      </button>
+      {/* // another way to handle click event */}
+      <button onClick={() => console.log('clicked')}>plus</button>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+      <button onClick={setCounter(counter + 1)}> 
+        plus
+      </button>
+    </div>
+  )
+}
+
+// Event handler is a function
+const AppEventHandlerFunction = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+// Passing State  to child components
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const AppStateToChild = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  )
+}
+
+// Changes in state cause re-rendering
+const AppChangesRendering = () => {
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  )
+} 
--- Part-1/part-c-component-state-and-event-handlers/src/main.jsx
+++ Part-1/part-c-component-state-and-event-handlers/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+// For re-rendering example
+
+// import ReactDOM from 'react-dom/client'
+
+// import AppRender from './App'
+
+// let counter = 1
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <AppRender counter={counter} />
+// )
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
